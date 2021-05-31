1c83ffa6f85077ba02ae45c3e5cb947c0c800dc8
Milan M2 <mmeldo@gmail.com>
2021-05-31 23:44:11 +0200

    add desc to chat command options

--- Components/GameBoard/GameChat.jsx
+++ Components/GameBoard/GameChat.jsx
@@ -1,59 +1,60 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import $ from 'jquery';
 
 import Messages from './Messages';
 import Typeahead from '../Form/Typeahead';
+import { Highlighter } from 'react-bootstrap-typeahead';
 
-const commands = [ 
-    '/ace ',
-    '/add-keyword ',
-    '/add-card ',
-    '/attach ',
-    '/bullets ',
-    '/blank ',
-    '/bounty ',
-    '/cancel-prompt ',
-    '/cancel-shootout ',
-    '/clear-shooter ',
-    '/clear-suit ',
-    '/clear-effects ',
-    '/control ',
-    '/discard-deck ',
-    '/discard-random ',
-    '/disconnectme ',
-    '/draw ',
-    '/give-control ',
-    '/hand-rank ',
-    '/inf ',
-    '/join-posse ',
-    '/join-without-move ',
-    '/kung-fu ',
-    '/look-deck ',
-    '/move ',
-    '/pass ',
-    '/pull ',
-    '/pull kf',
-    '/rematch ',
-    '/remove-from-game ',
-    '/remove-from-posse ',
-    '/remove-keyword ',
-    '/reset-abilities ',
-    '/reset-stats ',
-    '/reveal-deck ',
-    '/reveal-hand ',
-    '/shooter stud',
-    '/shooter draw',
-    '/shuffle-discard ',
-    '/skill blessed ',
-    '/skill huckster ',
-    '/skill shaman ',
-    '/skill mad ',
-    '/suit ',
-    '/token ',
-    '/unblank ',
-    '/use ',
-    '/value '
+const commands = [
+    { label: '/ace ', desc: 'Ace a card' },
+    { label: '/add-keyword ', desc: 'Add keyword to a card' },
+    { label: '/add-card ', syntax: '/add-card [code | title]', desc: 'Add card to the top of the Draw deck' },
+    { label: '/attach ', desc: '' },
+    { label: '/bullets ', desc: '' },
+    { label: '/blank ', desc: '' },
+    { label: '/bounty ', desc: '' },
+    { label: '/cancel-prompt ', desc: '' },
+    { label: '/cancel-shootout ', desc: '' },
+    { label: '/clear-shooter ', desc: '' },
+    { label: '/clear-suit ', desc: '' },
+    { label: '/clear-effects ', desc: '' },
+    { label: '/control ', desc: '' },
+    { label: '/discard-deck ', desc: '' },
+    { label: '/discard-random ', desc: '' },
+    { label: '/disconnectme ', desc: '' },
+    { label: '/draw ', desc: '' },
+    { label: '/give-control ', desc: '' },
+    { label: '/hand-rank ', desc: '' },
+    { label: '/inf ', desc: '' },
+    { label: '/join-posse ', desc: '' },
+    { label: '/join-without-move ', desc: '' },
+    { label: '/kung-fu ', desc: '' },
+    { label: '/look-deck ', desc: '' },
+    { label: '/move ', desc: '' },
+    { label: '/pass ', desc: '' },
+    { label: '/pull ', desc: '' },
+    { label: '/pull kf', desc: '' },
+    { label: '/rematch ', desc: '' },
+    { label: '/remove-from-game ', desc: '' },
+    { label: '/remove-from-posse ', desc: '' },
+    { label: '/remove-keyword ', desc: '' },
+    { label: '/reset-abilities ', desc: '' },
+    { label: '/reset-stats ', desc: '' },
+    { label: '/reveal-deck ', desc: '' },
+    { label: '/reveal-hand ', desc: '' },
+    { label: '/shooter stud', desc: '' },
+    { label: '/shooter draw', desc: '' },
+    { label: '/shuffle-discard ', desc: '' },
+    { label: '/skill blessed ', desc: '' },
+    { label: '/skill huckster ', desc: '' },
+    { label: '/skill shaman ', desc: '' },
+    { label: '/skill mad ', desc: '' },
+    { label: '/suit ', desc: '' },
+    { label: '/token ', desc: '' },
+    { label: '/unblank ', desc: '' },
+    { label: '/use ', desc: '' },
+    { label: '/value ', desc: '' }
 ];
 
 class GameChat extends React.Component {
@@ -63,6 +64,7 @@ class GameChat extends React.Component {
         this.onInputChange = this.onInputChange.bind(this);
         this.onKeyPress = this.onKeyPress.bind(this);
         this.onScroll = this.onScroll.bind(this);
+        this.renderMenuItemChildren = this.renderMenuItemChildren.bind(this);
 
         this.state = {
             canScroll: true,
@@ -80,6 +82,19 @@ class GameChat extends React.Component {
         if(this.state.canScroll) {
             $(this.refs.messagePanel).scrollTop(999999);
         }
+    }
+
+    renderMenuItemChildren(option, { text }) {
+        return (<Fragment>
+            <Highlighter search={ text }>
+                { option.syntax || option.label }
+            </Highlighter>
+            <div>
+                <small>
+                    { option.desc }
+                </small>
+            </div>
+        </Fragment>);
     }
 
     onScroll() {
@@ -125,8 +140,8 @@ class GameChat extends React.Component {
                     <Messages messages={ this.props.messages } onCardMouseOver={ this.props.onCardMouseOver } onCardMouseOut={ this.props.onCardMouseOut } />
                 </div>
                 <form className='form chat-form'>
-                    <Typeahead ref='message' options={ commands } emptyLabel={ '' } minLength={ this.state.minLength } dropup
-                        placeholder='Chat...' onKeyDown={ this.onKeyPress } onInputChange={ this.onInputChange } />
+                    <Typeahead ref='message' options={ commands } labelKey={ option => option.label } emptyLabel={ '' } minLength={ this.state.minLength } dropup
+                        placeholder='Chat...' onKeyDown={ this.onKeyPress } onInputChange={ this.onInputChange } renderMenuItemChildren={ this.renderMenuItemChildren } />
                 </form>
             </div>);
     }
