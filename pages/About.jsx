33896861a8fbec870883e7b68adcae2e4db8ec1d
AaronSchnabel <aaron.schnabel@gmail.com>
2021-08-16 14:04:46 -0700

    Update How to Play page (#72)
    Co-authored-by: Milan M2 <mmeldo@gmail.com>

--- pages/About.jsx
+++ pages/About.jsx
@@ -11,7 +11,7 @@ class About extends React.Component {
                     <a className='btn btn-danger btn-lg pull-right' target='_blank' href='https://github.com/townteki/townsquare/issues'>Report Problems</a>
                     <h3>What is this?</h3>
 
-                    <p>This site was setup to allow you to play Doomtown card game, a Pine Box Entertainment (PBE) in your browser.</p>
+                    <p>This site was setup to allow you to play Doomtown, a Pine Box Entertainment (PBE) game, in your browser.</p>
 
                     <h3>That's pretty cool!  But how does any of this work?</h3>
                     <p>Head on over to the <Link href='/how-to-play'>How To Play guide</Link> for a thorough explanation.</p>
@@ -31,15 +31,22 @@ class About extends React.Component {
                 status list above to have a look at what needs to be done.  If you want to join the dev discord, or ask any other question, send me a note on here, over at&nbsp;
                     </p>
 
-                    <h4>Donations</h4>
-                    Possible donations here.
+                    <h2>Patreon</h2>
+                    <p>Become a Patron of Pinebox Entertainment <a href='https://www.patreon.com/pineboxentertainment' target='blank'>here</a> and help support Doomtown Online!</p>
+                    <p>Patreon subscriptions help offset the cost of running the Doomtown Online server, domain registrations, etc and help support PBE so that they can continue making exciting games for everyone to enjoy!</p>
 
                     <h2>Special Thanks</h2>
-                    <p>Special thanks here.
+                    <p>Special thanks go out to Shane Hensley and all the fine folks at Pinnacle Entertainment Group for creating the wonderful world of Deadlands, and allowing us to play in their sandbox.
                     </p>
+                    <p>Also, a very special thank you to David Lapp and the rest of the Pinebox Entertainment crew for revitalizing this game and allowing us to bring Doomtown online for all to enjoy.</p>
 
                     <h2>Additional Notes</h2>
-                    <p>Additional notes here.
+                    <p>
+                    </p>
+
+                    <h2>Copyright Information</h2>
+                    <p>
+                    Deadlands: The Weird West, and Doomtown Artwork, logos, and the Pinnacle Logo are all &copy; Great White Games, LLC; DBA Pinnacle Entertainment Group and used with permission. Pine Box Entertaiment and the Pinebox logo are &copy; Pine Box Entertainment, LLC 2021.
                     </p>
                 </Panel>
             </div>
